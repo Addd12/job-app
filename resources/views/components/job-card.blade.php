f5a5ec040f3eb0c76b62ccb51e0acc3c29b7187d
Addd12 <addd.github@gmail.com>
2024-09-26 22:58:58 +0200

    d27 setup navbar, job card and tags using tailwindcss

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>video producer</h3>
+        <p>Full time - From 70,000 euro</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
