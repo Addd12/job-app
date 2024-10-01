27359aa2ef46811025344c0b164fe14fc13af757
Addd12 <addd.github@gmail.com>
2024-10-01 22:04:29 +0200

    d30 added authentication, job controller, search, handling images

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Job;
+
+class SearchController extends Controller
+{
+    public function __invoke(){
+        $jobs = Job::query()
+        ->with(['employers', 'tags'])
+        ->where('title', 'LIKE', '%'.request('q').'%')
+        ->get();
+        
+        return view('results', ['jobs' => $jobs]);
+    }
+}
